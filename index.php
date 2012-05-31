8be22e298eadc538a51888d0e3b01ce9494b5382
Matt Moretti <werebus@gmail.com>
2012-05-31 15:18:59 -0400

    Moved database info to a config file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOSTNAME', 'TBA');
-define('DB_USERNAME', 'TBA');
-define('DB_PASSWORD', 'TBA');
+ob_start();
+include './config/database.php';
+ob_end_clean();
 
 ini_set('display_errors', 0);
 ini_set('error_log', 'errorlog.txt');
